c0ceb69d6305e52adb5ffde1a8eda4bfc18a1729
Harry <shaikmohammadibrahim311@gmail.com>
2024-09-08 12:12:47 +0530

    no message

--- RSA-POC-2/ct-calculator-service/src/main/java/com/ct/calculator/ct_calculator_service/controller/CalculatorController.java
+++ RSA-POC-2/ct-calculator-service/src/main/java/com/ct/calculator/ct_calculator_service/controller/CalculatorController.java
@@ -1,6 +1,7 @@
 package com.ct.calculator.ct_calculator_service.controller;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ct.calculator.ct_calculator_service.constans.EndPoint;
 import com.ct.calculator.ct_calculator_service.controller.service.CalculatorService;
 import com.ct.calculator.ct_calculator_service.controller.service.ValidateSignature;
+import com.ct.calculator.ct_calculator_service.controller.service.VerifyRsa;
 import com.ct.calculator.ct_calculator_service.pojo.InputRequest;
 import com.ct.calculator.ct_calculator_service.pojo.InputRequestDTO;
 import com.ct.calculator.ct_calculator_service.pojo.ResultResponse;
 import com.ct.calculator.ct_calculator_service.pojo.ResultResponseDTO;
+import com.ct.calculator.ct_calculator_service.util.HmacSHA256Util2;
+import com.ct.calculator.ct_calculator_service.util.pojo.SendRequestAddNums;
 
 @RestController
 @RequestMapping(EndPoint.CALC_MAPPING)
@@ -32,33 +36,43 @@ public class CalculatorController {
     @Autowired
     private ValidateSignature signatureValid;
 
+    @Autowired
+    private HmacSHA256Util2 hmacSHa;
     
+    @Autowired
+    private VerifyRsa verifyRsa;
     @PostMapping(EndPoint.ADD_MAPPING)
-    public ResponseEntity<ResultResponse> getSum(@RequestHeader("signature") String signature, @RequestBody InputRequest request) {
-        try {
-            // Validate the signature
-            boolean isValidSignature = signatureValid.validateSignature(signature,request);
-            if (!isValidSignature) {
-                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-            }
-
-            // Map request to DTO
-            InputRequestDTO inputRequestDTO = modelMapper.map(request, InputRequestDTO.class);
-
-            // Perform the addition operation
-            ResultResponseDTO resultResponseDTO = calculatorService.addsum(inputRequestDTO);
-
-            // Map the result DTO to the response entity
-            ResultResponse result = modelMapper.map(resultResponseDTO, ResultResponse.class);
-            return new ResponseEntity<>(result, HttpStatus.OK);
-
-        } catch (Exception e) {
-            // Handle exceptions and return appropriate response
-            e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ResultResponse> getSum(@RequestHeader("signature") String signature, @RequestBody SendRequestAddNums request) {
+        // Validate the signature
+        boolean isValidSignature = signatureValid.validateSignature(signature, request);
+        if (!isValidSignature) {
+            // Throw your custom ValidationException with appropriate parameters
+        throw new 
+        com.ct.calculator.ct_calculator_service.exception.
+        ValidationException(HttpStatus.ACCEPTED, "2000","Data Altered");
         }
-        
+         String rsaSignature=request.getSignature();
+         
+         
+        // Map request to DTO
+        InputRequestDTO inputRequestDTO = request.getInputRequestDTO();
+        boolean verifyRsawithPublicKey = verifyRsa.verifyRsawithPublicKey(rsaSignature,inputRequestDTO);
+        // Perform the addition operation
+        //System.out.println("RSA----->"+verifyRsawithPublicKey);
+        if (!verifyRsawithPublicKey) {
+            // Throw your custom ValidationException with appropriate parameters
+        throw new 
+        com.ct.calculator.ct_calculator_service.exception.
+        ValidationException(HttpStatus.ACCEPTED, "2002","Signature Altered");
+        }
+        ResultResponseDTO resultResponseDTO = calculatorService.addsum(inputRequestDTO);
+
+        // Map the result DTO to the response entity
+        ResultResponse result = modelMapper.map(resultResponseDTO, ResultResponse.class);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
+
+
     
     @GetMapping(EndPoint.CALL_SERVICE)
     public String getCall() {
--- RSA-POC-2/ct-calculator-service/src/main/java/com/ct/calculator/ct_calculator_service/controller/service/ValidateSignature.java
+++ RSA-POC-2/ct-calculator-service/src/main/java/com/ct/calculator/ct_calculator_service/controller/service/ValidateSignature.java
@@ -1,9 +1,9 @@
 package com.ct.calculator.ct_calculator_service.controller.service;
 
-import com.ct.calculator.ct_calculator_service.pojo.InputRequest;
+import com.ct.calculator.ct_calculator_service.util.pojo.SendRequestAddNums;
 
 public interface ValidateSignature {
 
-	boolean validateSignature(String signature, InputRequest request);
+	boolean validateSignature(String signature, SendRequestAddNums request);
 
 }
